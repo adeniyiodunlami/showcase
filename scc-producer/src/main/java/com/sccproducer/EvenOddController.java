1fb18b01a1f5d8d6b5f1956a2a9c7d1848a243c6
Neeraja Vinoba <neeraja.vinoba@fiixsoftware.com>
2020-08-26 17:27:09 -0400

    Spike on spring cloud contract producer and consumer

---
+++ scc-producer/src/main/java/com/sccproducer/EvenOddController.java
@@ -0,0 +1,14 @@
+package com.sccproducer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvenOddController
+{
+	@GetMapping("/validate/even-number")
+	public String isNumberPrime(@RequestParam("number") Integer number) {
+		return number % 2 ==0? "Even": "Not Even";
+	}
+}
---
+++ scc-producer/src/main/java/com/sccproducer/SCCProducerApp.java
@@ -0,0 +1,11 @@
+package com.sccproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SCCProducerApp
+{
+	public static void main(String args[]) {
+		SpringApplication.run(SCCProducerApp.class, args);
+	}
+}
